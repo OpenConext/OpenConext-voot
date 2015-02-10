74d1542c1a03411b5c64b61c7d6a2aeacc5314b2
Lars Vonk <lars.vonk@gmail.com>
2015-02-10 11:42:23 +0100

    Renamed vootservice package to voot.
    Formatted codebase according to .editorconfig.

---
+++ src/test/java/voot/VootControllerTest.java
@@ -0,0 +1,22 @@
+package voot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VootControllerTest {
+
+  private VootController subject;
+
+  @Before
+  public void before() {
+    subject = new VootController();
+  }
+
+  @Test
+  public void testMyGroups() throws Exception {
+
+    assertNotNull(subject.myGroups());
+  }
+}
