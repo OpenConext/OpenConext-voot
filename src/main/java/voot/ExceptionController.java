0e57025685f69a0d518dc0810980ec17f73670f0
Lars Vonk <lars.vonk@gmail.com>
2015-02-12 15:59:59 +0100

    Added exception controller to at least log all exceptions.

---
+++ src/main/java/voot/ExceptionController.java
@@ -0,0 +1,19 @@
+package voot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionController {
+  private final static Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+  @ExceptionHandler(Exception.class)
+  public ModelAndView onException(Exception exception) {
+    logger.error("Unexpected exception occurred", exception);
+    return new ModelAndView("error");
+  }
+
+}
