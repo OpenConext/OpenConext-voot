67c1c1d9b3cc68a05a8c6eee3488a47ffe8197d3
Hans Westerbeek <hans@enhance-ict.com>
2015-02-25 16:23:04 +0100

    Be naiver.

--- src/main/java/voot/oauth/ExternalGroupsQuerier.java
+++ src/main/java/voot/oauth/ExternalGroupsQuerier.java
@@ -1,10 +1,6 @@
 package voot.oauth;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 public class ExternalGroupsQuerier {
@@ -18,20 +14,10 @@ public class ExternalGroupsQuerier {
   }
 
   public List<Voot2Group> getMyGroups(String uid, String schacHomeOrganization) {
-
-    ForkJoinPool forkJoinPool = new ForkJoinPool(groupClients.size());
-    try {
-      return forkJoinPool.submit(() -> this.groupClients.parallelStream()
-        .filter(client -> client.isAuthorative(schacHomeOrganization))
-        .map(client -> client.getMemberships(uid, schacHomeOrganization))
-        .flatMap(lists -> lists.stream())
-        .collect(Collectors.toList())
-      ).get(timeoutMillis, TimeUnit.MILLISECONDS);
-    } catch (InterruptedException | ExecutionException e) {
-      e.printStackTrace();
-    } catch (TimeoutException e) {
-      e.printStackTrace();
-    }
-    throw new RuntimeException("hmm");
+    return this.groupClients.parallelStream()
+      .filter(client -> client.isAuthorative(schacHomeOrganization))
+      .map(client -> client.getMemberships(uid, schacHomeOrganization))
+      .flatMap(lists -> lists.stream())
+      .collect(Collectors.toList());
   }
 }
