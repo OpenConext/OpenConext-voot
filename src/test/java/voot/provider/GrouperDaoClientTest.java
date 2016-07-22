d23299280f093420fad3b9d0cfd86eca6590cc79
Thijs Kinkhorst <thijs@kinkhorst.com>
2016-07-22 14:32:39 +0000

    Add tests for group name, description and sourceid in GrouperDao.

--- src/test/java/voot/provider/GrouperDaoClientTest.java
+++ src/test/java/voot/provider/GrouperDaoClientTest.java
@@ -45,9 +45,32 @@ public class GrouperDaoClientTest {
     assertMembership(groups, PREFIX + "nl:surfnet:diensten:burr", Membership.MANAGER);
     assertMembership(groups, PREFIX + "nl:surfnet:diensten:managementvo", Membership.MANAGER);
     assertMembership(groups, PREFIX + "nl:surfnet:diensten:test123", Membership.MEMBER);
+
+    assertDisplayName(groups, PREFIX + "nl:surfnet:diensten:bassie_&_adriaan", "bassie & adriaan");
+    assertDisplayName(groups, PREFIX + "nl:surfnet:diensten:bazenteam", "Bazenteam");
+    assertDisplayName(groups, PREFIX + "nl:surfnet:diensten:burr", "burr");
+    assertDisplayName(groups, PREFIX + "nl:surfnet:diensten:test123", "test123");
+
+    assertDescription(groups, PREFIX + "nl:surfnet:diensten:bazenteam", "Eh");
+    assertDescription(groups, PREFIX + "nl:surfnet:diensten:burr", "fffff");
+    assertDescription(groups, PREFIX + "nl:surfnet:diensten:test123", "Testteam");
+
+    assertSourceID(groups, PREFIX + "nl:surfnet:diensten:bazenteam", "grouper");
   }
 
   private void assertMembership(List<Group> groups, String groupId, Membership membership) {
-    assertEquals(groupId, groups.stream().filter(group -> group.id.equals(groupId)).collect(toList()).get(0).membership, membership);
+    assertEquals(groupId, membership, groups.stream().filter(group -> group.id.equals(groupId)).collect(toList()).get(0).membership);
+  }
+
+  private void assertDisplayName(List<Group> groups, String groupId, String displayName) {
+    assertEquals(groupId, displayName, groups.stream().filter(group -> group.id.equals(groupId)).collect(toList()).get(0).displayName);
+  }
+
+  private void assertDescription(List<Group> groups, String groupId, String description) {
+    assertEquals(groupId, description, groups.stream().filter(group -> group.id.equals(groupId)).collect(toList()).get(0).description);
+  }
+
+  private void assertSourceID(List<Group> groups, String groupId, String sourceID) {
+    assertEquals(groupId, sourceID, groups.stream().filter(group -> group.id.equals(groupId)).collect(toList()).get(0).sourceID);
   }
 }
